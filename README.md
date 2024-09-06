7e91f4bffe6ffcaeacd2c484c9a52cb6e62fe95f
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2024-09-06 02:26:28 -0400

    chore: install and set up Tailwind CSS

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# conform-demo
-Conform Demo with Remix - A demonstration project showcasing form validation and error handling using the Conform library in a Remix application.
+# Conform Demo with Remix
+
+A demonstration project showcasing form validation and error handling using the Conform library in a Remix application.
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
+import { vitePlugin as remix } from '@remix-run/dev';
+import path from 'path';
 
 export default defineConfig({
   plugins: [remix()],
+  css: {
+    postcss: './postcss.config.js',
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'app'), // Set '~' to alias to the 'app' folder
+    },
+  },
 });
