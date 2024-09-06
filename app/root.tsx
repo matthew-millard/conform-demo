a8754a24ed6e6ff79e5e33208c37fe6a66d7aa0f
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2024-09-06 12:42:17 -0400

    feat: add signup, login, and splat routes using Remix conventions

--- app/root.tsx
+++ app/root.tsx
@@ -1,23 +1,29 @@
-import { Links, Meta, Outlet, Scripts } from '@remix-run/react';
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 import styles from '~/tailwind.css?url';
 
 export const links = () => {
   return [{ rel: 'stylesheet', href: styles }];
 };
 
-export default function App() {
+export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <link rel="icon" href="data:image/x-icon;base64,AA" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body>
-        <h1 className="text-3xl font-bold">Hello World!</h1>
-        <Outlet />
+        {children}
+        <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
+}
+
+export default function App() {
+  return <Outlet />;
 }
---
+++ app/routes/_auth.signup.tsx
@@ -0,0 +1,7 @@
+export default function SignupRoute() {
+  return (
+    <div>
+      <h1>Signup</h1>
+    </div>
+  );
+}
