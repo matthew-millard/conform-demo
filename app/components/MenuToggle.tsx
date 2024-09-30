9d0b07aa1e0330a1b1f7162302570bf7ea06dd39
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2024-09-30 12:47:59 -0400

    refactor: update primary color to dodger blue

--- app/components/MenuToggle.tsx
+++ app/components/MenuToggle.tsx
@@ -2,7 +2,7 @@ import { PopoverButton } from '@headlessui/react';
 
 export default function MenuToggle({ children }: { children: React.ReactNode }) {
   return (
-    <PopoverButton className="relative inline-flex border border-transparent items-center justify-center rounded-md bg-primary p-2 text-on-primary focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary">
+    <PopoverButton className="relative inline-flex border border-transparent items-center justify-center rounded-md  p-2 text-on-surface focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary">
       {children}
     </PopoverButton>
   );
