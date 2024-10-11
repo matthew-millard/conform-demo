10e621fef3517862e8133ea8a7cf7f3186b5dee5
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2024-10-11 01:24:24 -0400

    feat: shift focus back to current password input on successful password update

--- app/components/InputText.tsx
+++ app/components/InputText.tsx
@@ -1,13 +1,16 @@
+import { forwardRef } from 'react';
+
 type InputTextProps = {
   fieldAttributes: React.InputHTMLAttributes<HTMLInputElement>;
 };
 
-export default function InputText({ fieldAttributes }: InputTextProps) {
-  return (
-    <input
-      {...fieldAttributes}
-      key={null}
-      className="block w-full rounded-md bg-transparent border-0 py-1.5 pl-3 text-on-surface shadow-sm ring-1 ring-inset ring-around-surface focus:outline-none  focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6 aria-[invalid]:ring-error"
-    />
-  );
-}
+const InputText = forwardRef<HTMLInputElement, InputTextProps>(({ fieldAttributes }, ref) => (
+  <input
+    {...fieldAttributes}
+    ref={ref}
+    key={null}
+    className="block w-full rounded-md bg-transparent border-0 py-1.5 pl-3 text-on-surface shadow-sm ring-1 ring-inset ring-around-surface focus:outline-none  focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6 aria-[invalid]:ring-error"
+  />
+));
+
+export default InputText;
